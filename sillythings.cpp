1879a221b71f4e89c8ab8f720084670f8850917f
Eitan Adler <lists@eitanadler.com>
2011-06-25 20:06:40 -0400

    I SEE YOUR SHIP; the cake is a lie

--- sillythings.cpp
+++ sillythings.cpp
@@ -1,15 +1,39 @@
 #include <SFML/System.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
 #include <iostream>
 
 int main()
 {
-    sf::Clock Clock;
-    while (Clock.GetElapsedTime() < 5.f)
-    {
-        std::cout << Clock.GetElapsedTime() << std::endl;
-        sf::Sleep(0.5f);
-    }
 
-    return 0;
+	// Create the main window
+	sf::RenderWindow App(sf::VideoMode(800, 600), "SFML window");
+	
+	sf::Image Image;
+	if (!Image.LoadFromFile("ship.png"))
+		return EXIT_FAILURE;
+	sf::Sprite Sprite(Image);
+
+	while (App.IsOpened())
+	{
+		 // Process events
+		 sf::Event Event;
+		 while (App.GetEvent(Event))
+		 {
+			 // Close window : exit
+			 if (Event.Type == sf::Event::Closed)
+				 App.Close();
+		 }
+
+		 // Clear screen
+         App.Clear(sf::Color(200,0,0));
+
+		 // Draw the sprite
+		 App.Draw(Sprite);
+
+		App.Display();
+	}
+
+	return EXIT_SUCCESS;
 }
 
